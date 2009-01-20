a302301ce0d0d5eeff515918802441da61c996c5
freakboy3742 <freakboy3742@4c0570ac-e6c1-11dd-ab07-b19b4dad1b3d>
2009-01-20 10:52:53 +0000

    Initial code checkin

---
+++ trunk/pyspamsum.c
@@ -0,0 +1,95 @@
+/**
+ * PySpamsum v1.0.0
+ *
+ * A Python wrapper around the spamsum library written by
+ * Andrew Tridgell.
+ *
+ * Copyright 2009 Russell Keith-Magee <russell@keith-magee.com>
+ */
+#include <Python.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int edit_distn(char *from, int from_len, char *to, int to_len);
+char *spamsum(const unsigned char *in, size_t length, unsigned int flags, unsigned int bsize);
+unsigned int spamsum_match(const char *str1, const char *str2);
+
+PyObject *py_edit_distance(PyObject *self, PyObject *args)
+{
+    PyObject *result = NULL;
+    int distance;
+
+    char *from, *to;
+    int from_len, to_len;
+
+    if (!PyArg_ParseTuple(args, "s#s#", &from, &from_len, &to, &to_len))
+    {
+        return NULL;
+    }
+
+    distance = edit_distn(from, from_len, to, to_len);
+    result = Py_BuildValue("i", distance);
+
+    return result;
+}
+
+
+PyObject *py_spamsum(PyObject *self, PyObject *args)
+{
+    PyObject *result = NULL;
+    char *sum;
+
+    unsigned char *in;
+    size_t length;
+    unsigned int flags, bsize;
+
+    flags = 0;
+    bsize = 0;
+
+    if (!PyArg_ParseTuple(args, "s#|ii", &in, &length, &flags, &bsize))
+    {
+        return NULL;
+    }
+    sum = spamsum(in, length, flags, bsize);
+    result = Py_BuildValue("s", sum);
+
+    return result;
+}
+
+PyObject *py_match(PyObject *self, PyObject *args)
+{
+    PyObject *result = NULL;
+    unsigned int match;
+    char *str1, *str2;
+
+    if (!PyArg_ParseTuple(args, "ss", &str1, &str2))
+    {
+        return NULL;
+    }
+
+    match = spamsum_match(str1, str2);
+    result = Py_BuildValue("i", match);
+
+    return result;
+}
+
+
+static PyMethodDef methods[] = {
+    {"edit_distance", py_edit_distance, METH_VARARGS,
+        "Calculate the edit distance between two strings."
+    },
+    {"spamsum", py_spamsum, METH_VARARGS,
+        "Calculate the spamsum of a string."
+    },
+    {"match", py_match, METH_VARARGS,
+        "Given two spamsum strings return a value indicating the degree to which they match."
+    },
+    {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+initspamsum()
+{
+    (void) Py_InitModule("spamsum", methods);
+}
---
+++ trunk/setup.py
@@ -0,0 +1,55 @@
+from distutils.core import setup, Extension
+
+setup(name = "spamsum",
+    version = "1.0.0",
+    author = "Russell Keith-Magee",
+    author_email = "russell@keith-magee.com",
+    url = 'http://code.google.com/p/pyspamsum/',
+    license = "New BSD",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+    platforms = ["any"],
+    description = "A Python wrapper for Andrew Tridgell's spamsum algorithm",
+    long_description = """
+spamsum is a fuzzy hash specifically designed for hashing email messages
+to detect if they are SPAM. The spamsum utility includes the ability to
+generate the spamsum hash and check a new message against a existing set
+of hashes to find a match.
+
+pyspamsum is a Python wrapper for the core API of spamsum.
+
+The original spamsum code has been licensed under the terms of the
+the Perl Artistic License. It has been slightly modified
+
+The original code is Copyright Andrew Tridgell <tridge@samba.org> 2002.
+It forms part of Andrew's junkcode, and is available here:
+
+    http://www.samba.org/junkcode/#spamsum
+
+The spamsum code in this project is derived from an updated version that
+was published at Linux.conf.au 2004:
+
+    http://linux.anu.edu.au/linux.conf.au/2004/papers/junkcode/spamsum
+
+For details on spamsum itself, please see the spamsum README:
+
+    http://samba.org/ftp/unpacked/junkcode/spamsum/README
+""",
+    classifiers = [
+
+    ],
+    ext_modules = [
+        Extension(
+            "spamsum", [
+                "pyspamsum.c",
+                "spamsum.c",
+                "edit_dist.c",
+            ]
+        )
+    ]
+)
